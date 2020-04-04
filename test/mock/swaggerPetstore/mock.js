6882f952f65d87ed965c64cdfd08699d0b990a0f
yeyan1996 <1996yeyan@gmail.com>
2020-04-04 20:45:27 +0800

    chore: 添加 travis å作为 CI

--- test/mock/swaggerPetstore/mock.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const mock = {};
-fs.readdirSync(".")
-  .filter(file => file.endsWith(".json"))
-  .map(file => {
-    Object.assign(mock, require(`./${file}`));
-  });
-exports.mock = mock;
-  
