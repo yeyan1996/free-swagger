c21232b632ba8f5dca5f9541db7962acd6e50b83
yeyan1996 <1366792281@qq.com>
2020-08-07 14:03:59 +0800

    Publish
     - free-swagger-client@2.4.0
     - free-swagger-extends@2.4.0
     - free-swagger@2.4.0

--- packages/extends/dist/userScript.js
+++ packages/extends/dist/userScript.js
@@ -3,7 +3,7 @@
 // @name  free-swagger-extends
 // @namespace  http://tampermonkey.net/
 // @description  swagger 油猴脚本扩展工具
-// @version  2.3.3
+// @version  2.4.0
 
 // @include  /swagger/
 
@@ -24,5 +24,5 @@ function createScripTag(src) {
     document.body.append(createDom("script", {src}));
 }
 
-createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-extends/dist/js/app.ff5fa5b1.js")
+createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-extends/dist/js/app.5e2950ab.js")
 //  createScripTag("http://localhost:8888/js/app.js") // 本地调试用
