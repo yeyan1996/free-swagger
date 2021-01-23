fd2762a1ba82b48205f1eaebb530ec46d19d3b08
yeyan1996 <1366792281@qq.com>
2021-01-23 09:20:12 +0800

    fix: bin error

--- packages/extends/dist/userScript.js
+++ packages/extends/dist/userScript.js
@@ -25,5 +25,5 @@ function createScripTag(src) {
     document.body.append(createDom("script", {src}));
 }
 
-createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-extends/dist/js/app.5cc95ef0.js")
+createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-extends/dist/js/app.c5e98262.js")
 //  createScripTag("http://localhost:8888/js/app.js") // 本地调试用
--- packages/server/src/bin.ts
+++ packages/server/src/bin.ts
@@ -1,1 +1,2 @@
+#!/usr/bin/env node
 console.log('free-swagger 命令已经废弃，请替换为 npx free-swagger-cli')
