011a31f1c60920b9ccf19cba1e69fb89f07d6473
yeyan1996 <1366792281@qq.com>
2021-01-23 09:57:08 +0800

    chore: remove mock file

--- packages/extends/dist/userScript.js
+++ packages/extends/dist/userScript.js
@@ -25,5 +25,5 @@ function createScripTag(src) {
     document.body.append(createDom("script", {src}));
 }
 
-createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-extends/dist/js/app.81e5b20f.js")
+createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-extends/dist/js/app.49aeabdc.js")
 //  createScripTag("http://localhost:8888/js/app.js") // 本地调试用
