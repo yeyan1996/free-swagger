f2066827d2ac93e5a2bd9d4440e54ba41bacf7a9
zhouhaolei <zhouhaolei@bytedance.com>
2021-02-11 15:50:51 +0800

    refactor: change depencencies into each package

--- packages/extends/dist/userScript.js
+++ packages/extends/dist/userScript.js
@@ -25,5 +25,5 @@ function createScripTag(src) {
     document.body.append(createDom("script", {src}));
 }
 
-createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-extends/dist/js/app.d1e5ada2.js")
+createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-extends/dist/js/app.7ce7c5da.js")
 //  createScripTag("http://localhost:8888/js/app.js") // 本地调试用
