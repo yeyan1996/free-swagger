0a91f2f412b986d0b7eebc3c685020e5a27898d2
zhouhaolei <zhouhaolei@bytedance.com>
2021-02-13 11:29:49 +0800

    refactor: order package.json & remove unused babel config

--- packages/extends/dist/userScript.js
+++ packages/extends/dist/userScript.js
@@ -25,5 +25,5 @@ function createScripTag(src) {
     document.body.append(createDom("script", {src}));
 }
 
-createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-extends/dist/js/app.137723af.js")
+createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-extends/dist/js/app.3e017e21.js")
 //  createScripTag("http://localhost:8888/js/app.js") // 本地调试用
