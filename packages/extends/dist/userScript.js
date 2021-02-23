7ea598373cdfbe6d3d7bb11c7f5c5c6302a5dbea
zhouhaolei <zhouhaolei@bytedance.com>
2021-02-23 18:43:46 +0800

    Publish
     - free-swagger-cli@4.0.0
     - free-swagger-client@4.0.0
     - free-swagger-extends@4.0.0
     - free-swagger@4.0.0

--- packages/extends/dist/userScript.js
+++ packages/extends/dist/userScript.js
@@ -3,7 +3,7 @@
 // @name  free-swagger-extends
 // @namespace  http://tampermonkey.net/
 // @description  free-swagger for tampermonkey
-// @version  3.5.3
+// @version  4.0.0
 
 // @include  swagger
 // @include  /doc.html
@@ -25,5 +25,5 @@ function createScripTag(src) {
     document.body.append(createDom("script", {src}));
 }
 
-createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-extends/dist/js/app.644ed8ff.js")
+createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-extends/dist/js/app.aa72246a.js")
 //  createScripTag("http://localhost:8888/js/app.js") // 本地调试用
