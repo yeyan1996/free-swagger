5749eeb81175778b6e339bb3ac4727ddc7771d3b
zhouhaolei <zhouhaolei@bytedance.com>
2021-09-14 15:03:24 +0800

    chore: update version'

--- packages/userscript/dist/userScript.js
+++ packages/userscript/dist/userScript.js
@@ -25,5 +25,5 @@ function createScripTag(src) {
     document.body.append(createDom("script", {src}));
 }
 
-createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-userscript/dist/js/app.5cd527b7.js")
+createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-userscript/dist/js/app.c8780166.js")
 //  createScripTag("http://localhost:8888/js/app.js") // 本地调试用
