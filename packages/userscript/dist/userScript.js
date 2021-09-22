3051750ac1e141a8bf38edd73ae060c5fb873b9e
yeyan <zhouhaolei@bilibili.com>
2021-09-22 14:55:25 +0800

    Publish
     - free-swagger@5.3.2
     - free-swagger-cli@5.3.2
     - free-swagger-extension@5.3.2
     - free-swagger-userscript@5.3.2

--- packages/userscript/dist/userScript.js
+++ packages/userscript/dist/userScript.js
@@ -3,7 +3,7 @@
 // @name  free-swagger-userscript
 // @namespace  http://tampermonkey.net/
 // @description  free-swagger for tampermonkey
-// @version  5.3.1
+// @version  5.3.2
 
 // @include  /swagger
 // @include  /doc.html
@@ -25,5 +25,5 @@ function createScripTag(src) {
     document.body.append(createDom("script", {src}));
 }
 
-createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-userscript/dist/js/app.5b7cc816.js")
+createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-userscript/dist/js/app.b4cc37c2.js")
 //  createScripTag("http://localhost:8888/js/app.js") // 本地调试用
