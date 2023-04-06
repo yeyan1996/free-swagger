da8d042f5883ae83fa9e899e488750dc839e03c8
yeyan1996 <1996yeyan@gmail.com>
2023-04-06 14:37:40 +0800

    Publish
     - free-swagger@5.4.1
     - free-swagger-cli@5.4.1
     - free-swagger-core@5.4.1
     - free-swagger-extension@5.4.1
     - free-swagger-userscript@5.4.1

--- packages/userscript/dist/userScript.js
+++ packages/userscript/dist/userScript.js
@@ -3,7 +3,7 @@
 // @name  free-swagger-userscript
 // @namespace  http://tampermonkey.net/
 // @description  free-swagger for tampermonkey
-// @version  5.4.0
+// @version  5.4.1
 
 // @include  /swagger
 // @include  /doc.html
@@ -25,5 +25,5 @@ function createScripTag(src) {
     document.body.append(createDom("script", {src}));
 }
 
-createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-userscript/dist/js/app.90c5333f.js")
+createScripTag("https://cdn.jsdelivr.net/npm/free-swagger-userscript/dist/js/app.e36316a0.js")
 //  createScripTag("http://localhost:8888/js/app.js") // 本地调试用
